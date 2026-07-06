01bdcca64dafa0163ca22c4c54dcba157e2a5c08
ahmeteraldmr <ahmeteraldmr@example.com>
2026-07-06 23:54:41 +0300

    Redirect user to home page instead of login page when logging out

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -86,6 +86,6 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect('/');
     }
 }
